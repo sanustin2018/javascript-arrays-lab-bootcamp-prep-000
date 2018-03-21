3b571dcefbebcabf7f164dad37196980ed0ffd3d
sperrysz39@gmail.com <sperrysz39@gmail.com>
2018-03-21 15:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten(name) {
+ kittens.push(name)
+}
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+var kittens = ['Milo', 'Otis', 'Garfield']
+function appendKitten(name){
+  kittens.push(...name)
+}
